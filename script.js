d5810a9224c115bd44e3bc5c241141e4e083f769
rafibudiansyah <95408922+rafibudiansyah@users.noreply.github.com>
2023-10-23 20:02:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,60 @@
-const header = document.querySelector("header");
+/**
+ * Untuk membuat link bisa dicopy
+ */
+const actionLink = document.querySelectorAll(".link-card .link-action");
 
-window.addEventListener ("scroll", function() {
-    header.classList.toggle ("sticky", window.scrollY > 120);
+actionLink.forEach((action) => {
+  action.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(action.parentElement.getAttribute("href"));
+
+    /**
+     * Untuk memunculkan toast notification
+     */
+    document.getElementById("toast").innerHTML = `
+        <div class="toast-container">
+            <p>✅ Link <strong> ${action.parentElement.innerText} </strong> berhasil disalin!</p>
+        </div>
+    `;
+
+    /**
+     * Untuk menghilangkan toast notification
+     */
+
+    setTimeout(() => {
+      document
+        .querySelector("#toast .toast-container")
+        .classList.add("toast-gone");
+    }, 300);
+
+    setTimeout(() => {
+      document.querySelector("#toast .toast-container").remove();
+    }, 2000);
+  });
 });
 
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+/**
+ * Untuk ganti icon sosmed saat hover
+ */
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('active');
-};
+document.querySelectorAll(".sosmed i").forEach((sosmed) => {
+  sosmed.addEventListener("mouseenter", () => {
+    sosmed.classList.remove("ph");
+    sosmed.classList.add("ph-fill");
+  });
 
-window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navlist.classList.remove('active');
-};
+  sosmed.addEventListener("mouseleave", () => {
+    sosmed.classList.remove("ph-fill");
+    sosmed.classList.add("ph");
+  });
+});
 
+/**
+ * Animasi Text bergerak saat scroll
+ */
 
-
-
-
-const dynamicText = document.querySelector("h1 span");
-const words = ["Rafi Budiansyah", "Content Creator", "Web Development", "Computer"];
-
-// Variables to track the position and deletion status of the word
-let wordIndex = 0;
-let charIndex = 0;
-let isDeleting = false;
-
-const typeEffect = () => {
-    const currentWord = words[wordIndex];
-    const currentChar = currentWord.substring(0, charIndex);
-    dynamicText.textContent = currentChar;
-    dynamicText.classList.add("stop-blinking");
-
-    if (!isDeleting && charIndex < currentWord.length) {
-        // If condition is true, type the next character
-        charIndex++;
-        setTimeout(typeEffect, 90);
-    } else if (isDeleting && charIndex > 0) {
-        // If condition is true, remove the previous character
-        charIndex--;
-        setTimeout(typeEffect, 90);
-    } else {
-        // If word is deleted then switch to the next word
-        isDeleting = !isDeleting;
-        dynamicText.classList.remove("stop-blinking");
-        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-        setTimeout(typeEffect, 1200);
-    }
-}
-
-typeEffect();
+document.addEventListener("scroll", (e) => {
+  document.querySelector(".bg-text-animation").style.transform = `translateX(${
+    window.scrollY / 5
+  }px)`;
+});
